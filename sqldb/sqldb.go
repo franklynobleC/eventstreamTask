4775a372a3275f2cc798215f7e0ae96ce16ed0d8
franklynobleC <franklyessien01@gmail.com>
2022-11-15 08:24:53 -0500

    new db imp

--- sqldb/sqldb.go
+++ sqldb/sqldb.go
@@ -1,12 +1,12 @@
 
 package sqldb
 type EventStoreDB struct {
-	DB *sql.DB
+	// DB *
 }
 
 func NewEventStoreDB() (*EventStoreDB, error) {
 	// Connect to the "eventstoredb" database
-	dbEventStore, err := sql.Open("postgres", "postgresql://shijuvar@localhost:26257/eventstoredb?sslmode=disable")
+	// 
 	if err != nil {
 		return nil, err
 	}
@@ -17,10 +17,7 @@ func NewEventStoreDB() (*EventStoreDB, error) {
 
 func (eventstore *EventStoreDB) CreateEventStoreDBSchema() error {
 	// Create the "events" table.
-	if _, err := eventstore.DB.Exec(
-		"CREATE TABLE IF NOT EXISTS events (id string PRIMARY KEY, eventtype STRING, aggregateid STRING, aggregatetype STRING, eventdata JSONB, stream STRING)"); err != nil {
-		return err
-	}
+	
 	return nil
 }
 
@@ -28,33 +25,24 @@ func (eventstore *EventStoreDB) Close() {
 	eventstore.DB.Close()
 }
 
-type OrdersDB struct {
+type  target struct {
 	DB *sql.DB
 }
 
-func NewOrdersDB() (*OrdersDB, error) {
+func NewTarget() (*OrdersDB, error) {
 	// Connect to the "ordersdb" database
-	dbOrders, err := sql.Open("postgres", "postgresql://shijuvar@localhost:26257/ordersdb?sslmode=disable")
+	targetDB, err := .Open("")
 	if err != nil {
 		return nil, err
 	}
-	return &OrdersDB{
-		DB: dbOrders,
-	}, nil
+	
 }
-func (orders *OrdersDB) CreateOrdersDBSchema() error {
-	// Create the "orders" table.
-	if _, err := orders.DB.Exec(
-		"CREATE TABLE IF NOT EXISTS orders (id STRING PRIMARY KEY, customerid STRING, status STRING, createdon DATE, restaurantid STRING, amount FLOAT)"); err != nil {
-		return err
-	}
 
-	// Create the "orderitems" table.
-	if _, err := orders.DB.Exec(
-		"CREATE TABLE IF NOT EXISTS orderitems (id serial PRIMARY KEY, orderid STRING, customerid STRING, code STRING, name STRING, unitprice FLOAT, quantity INT )"); err != nil {
-		return err
-	}
-	return nil
+//TODO CREATE TARGET SCHEMA
+func (target *TargetUser) CreateTargetDBschema() error {
+	// Create the "orders" table.
+	
 }
-func (orders *OrdersDB) Close() {
-	orders.DB.Close()
+func (target *targetDB) Close() {
+	target.DB.Close()
+}
